393e9e775e37433220d2d0b50d242db971df1e6c
threadpoolexecutor <davidkor77@gmail.com>
2021-09-26 11:59:24 +0300

    remove SIGPOLL from MacOS and FreeBSD stacktrace

--- cpp_vk_lib/src/runtime/signal_handlers.cpp
+++ cpp_vk_lib/src/runtime/signal_handlers.cpp
@@ -7,11 +7,26 @@
 
 void runtime::setup_signal_handlers()
 {
+    std::vector<int> critical_signals;
+    std::vector<int> info_signals;
 #if defined(__FreeBSD__) || defined(__linux__) || defined(__APPLE__)
-    for (int sig : {SIGABRT, SIGALRM, SIGBUS,    SIGFPE,  SIGHUP,  SIGILL,
-                    SIGINT,  SIGKILL, SIGPIPE,   SIGQUIT, SIGSEGV, SIGTERM,
-                    SIGSTOP, SIGTSTP, SIGTTIN,   SIGTTOU, SIGPOLL, SIGPROF,
-                    SIGSYS,  SIGTRAP, SIGVTALRM, SIGXCPU, SIGXFSZ}) {
+    for (int sig : {SIGABRT, SIGALRM,   SIGBUS,  SIGFPE,  SIGHUP,  SIGILL,
+                    SIGINT,  SIGKILL,   SIGPIPE, SIGQUIT, SIGSEGV, SIGTERM,
+                    SIGSTOP, SIGTSTP,   SIGTTIN, SIGTTOU, SIGPROF, SIGSYS,
+                    SIGTRAP, SIGVTALRM, SIGXCPU, SIGXFSZ}) {
+        critical_signals.push_back(sig);
+    }
+#endif
+
+#if not defined(__APPLE__) && not defined(__FreeBSD__)
+    critical_signals.push_back(SIGPOLL);
+#endif
+
+#if defined(__FreeBSD__) || defined(__linux__) || defined(__APPLE__)
+    for (int sig : {SIGCHLD, SIGCONT, SIGUSR1, SIGUSR2, SIGURG}) {
+        info_signals.push_back(sig);
+    }
+    for (int sig : critical_signals) {
         signal(sig, [](int signal) {
             spdlog::critical(
                 "{} signal got, showing stacktrace dump and exit...",
@@ -20,7 +35,7 @@ void runtime::setup_signal_handlers()
             exit(-1);
         });
     }
-    for (int sig : {SIGCHLD, SIGCONT, SIGUSR1, SIGUSR2, SIGURG}) {
+    for (int sig : info_signals) {
         signal(sig, [](int signal) {
             spdlog::critical(
                 "{} signal got, showing stacktrace dump...",

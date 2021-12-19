67a54ab3c5250e65ae4b82a05f000ccf468131dd
epoll-reactor <>
2021-12-19 17:50:42 +0100

    update voice messages example

--- examples/upload_audio_message.cpp
+++ examples/upload_audio_message.cpp
@@ -1,19 +1,19 @@
-//#include "cpp_vk_lib/runtime/setup_logger.hpp"
-//#include "cpp_vk_lib/runtime/signal_handlers.hpp"
-//#include "cpp_vk_lib/vk/config/config.hpp"
-//#include "cpp_vk_lib/vk/methods/constructor.hpp"
-//#include "cpp_vk_lib/vk/events/wall_post_new.hpp"
-//
-//#include "simdjson.h"
-//#include "spdlog/spdlog.h"
-//
-//#include <iostream>
-//
-//inline bool cpp_vk_lib_curl_verbose = true;
+#include "cpp_vk_lib/runtime/setup_logger.hpp"
+#include "cpp_vk_lib/runtime/signal_handlers.hpp"
+#include "cpp_vk_lib/vk/config/config.hpp"
+#include "cpp_vk_lib/vk/methods/constructor.hpp"
+#include "cpp_vk_lib/vk/events/wall_post_new.hpp"
+
+#include "simdjson.h"
+#include "spdlog/spdlog.h"
+
+#include <iostream>
+#include <fstream>
+
+bool cpp_vk_lib_curl_verbose = false;
 
 int main(int argc, char* argv[])
 {
-    /*
     if (argc != 4) {
         std::cerr << "Usage: ./upload_audio_message <config.json> <file.mp3> <peer_id>" << std::endl;
         return 1;
@@ -33,17 +33,20 @@ int main(int argc, char* argv[])
         .perform_request();
     simdjson::dom::parser parser;
     const std::string upload_server(parser.parse(server)["response"]["upload_url"]);
-    runtime::network::request_context ctx;
-    ctx.io_filename = file_path;
-    ctx.upload_field = "file";
-    ctx.upload_content_type = "application/octet-stream";
-    ctx.io_server = upload_server;
-    ctx.output_needed = runtime::network::data_flow::require;
-    const auto upload_response = runtime::network::upload(ctx);
-    if (upload_response.error()) {
-        spdlog::error("Upload error: {}", upload_response.error());
+    namespace net = runtime::network;
+
+    auto [uploaded, upload_error] = runtime::network::upload(
+        upload_server,
+        "file",
+        "application/octet-stream",
+        file_path,
+        net::data_flow::require
+    );
+    if (upload_error) {
+        spdlog::error("Upload error");
+        return 1;
     }
-    const simdjson::dom::element parsed_upload = parser.parse(upload_response.value());
+    const simdjson::dom::element parsed_upload = parser.parse(uploaded);
     const std::string file(parsed_upload["file"]);
     if (file.empty()) {
         return 1;
@@ -61,15 +64,11 @@ int main(int argc, char* argv[])
             audio_message["id"].get_int64(),
             audio_message["link_ogg"].get_string(),
             audio_message["link_mp3"].get_string());
-    std::vector<vk::attachment::attachment_ptr_t> atts;
-    atts.emplace_back(std::move(doc));
     vk::method::user_constructor()
         .method("messages.send")
         .param("peer_id", std::to_string(2000000000L + std::stol(peer_id)))
-        .param("message", "")
         .param("random_id", "0")
-        .param("attachments", atts[0]->value())
-        .perform_request();
+        .param("attachment", doc->value())
+        .request_without_output();
     return 0;
-     */
 }

d6cefc4a2151d7941b132793f0b23ae1d24ba086
Lonely Dark <42666581+Lonely-Dark@users.noreply.github.com>
2022-09-27 22:28:53 +0300

    Update text.hpp

--- cpp_vk_lib/include/cpp_vk_lib/vk/keyboard/buttons/text.hpp
+++ cpp_vk_lib/include/cpp_vk_lib/vk/keyboard/buttons/text.hpp
@@ -11,22 +11,25 @@ namespace vk::keyboard::button {
 class text : public button::base
 {
 public:
-    text(color selected_color, std::string_view payload_data)
+    text(color selected_color, std::string_view payload_data, std::string_view text)
         : selected_color_(selected_color)
-        , payload_data_(payload_data) {}
+        , payload_data_(payload_data)
+        , text_(text){}
 
     std::string serialize() const override
     {
         const char* color = keyboard::get_color(selected_color_);
         return fmt::format(
-            R"__({{"action":{{"type":"text","payload":"{{\"button\":\"1\"}}","label":"{}"}},"color":"{}"}})__",
+            R"__({{"action":{{"type":"text","payload":"{}","label":"{}"}},"color":"{}"}})__",
             payload_data_,
+            text,
             color);
     }
 
 private:
     color selected_color_ = color::none;
     std::string payload_data_;
+    std::string text_;
 };
 
 }// namespace vk::keyboard::button
